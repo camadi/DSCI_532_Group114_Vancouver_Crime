aed21611a0cbb72b4f9cd9f7ec5565c546f63b5a
Chimaobi Amadi <maobiamadi@gmail.com>
2019-12-11 20:06:23 -0800

    Added plots to app

--- app.R
+++ app.R
@@ -63,8 +63,8 @@ pop_prop <- pop_prop %>%
         ))
 
 #We can get the years from the dataset to make ticks on the slider
-year_no_2019 <- pop_yr %>% filter(YEAR!=2019)
-yearMarks <- map(unique(year_no_2019$YEAR), as.character)
+
+yearMarks <- map(unique(mydata$YEAR), as.character)
 names(yearMarks) <- unique(year_no_2019$YEAR)
 yearSlider <- dccRangeSlider(
   id = "year",
@@ -75,12 +75,39 @@ yearSlider <- dccRangeSlider(
   value = list(2010, 2018)
 )
 
+neigh_Dropdown <- dccDropdown(
+  id = "neighbourhood",
+  # map/lapply can be used as a shortcut instead of writing the whole list
+  # especially useful if you wanted to filter by country!
+  options = map(
+    levels(mydata$NEIGHBOURHOOD), function(x){
+      list(label=x, value=x)
+    }),
+  value = levels(mydata$NEIGHBOURHOOD), #Selects all by default
+  multi = TRUE
+)
 
+type_Dropdown <- dccDropdown(
+  id = "type",
+  # map/lapply can be used as a shortcut instead of writing the whole list
+  # especially useful if you wanted to filter by country!
+  options = map(
+    levels(mydata$TYPE), function(x){
+      list(label=x, value=x)
+    }),
+  value = levels(mydata$TYPE), #Selects all by default
+  multi = TRUE
+)
 
-# type_lst <- unique(mydata$TYPE)
-# ngbrhd_lst <- unique(mydata$NEIGHBOURHOOD)
-# yr_lst <- unique(mydata$YEAR)
-make_charts <- function(type_lst, ngbrhd_lst, yr_lst){
+type_lst <- unique(mydata$TYPE)
+ngbrhd_lst <- unique(mydata$NEIGHBOURHOOD)
+yr_lst <- unique(mydata$YEAR)
+all_neig <- unique(mydata$NEIGHBOURHOOD)
+all_year <- unique(mydata$YEAR)
+all_types <- unique(mydata$TYPE)
+
+#Creating charts
+make_charts <- function(type_lst = [all_types], ngbrhd_lst = [all_neig], yr_lst = [all_year]){
     df <- mydata %>% 
         filter(TYPE %in% type_lst & NEIGHBOURHOOD %in% ngbrhd_lst & YEAR %in% yr_lst)
 
@@ -116,7 +143,7 @@ make_charts <- function(type_lst, ngbrhd_lst, yr_lst){
     crime_rate <- inner_join(crime_rate, pop_yr)
     crime_rate <- crime_rate %>% mutate(rate = (N/Population)*1000)
 
-
+    chart = {}
     chart[1] <- ggplot(MOY, aes(x=reorder(MONTH_NAME, MONTH), y=N)) + 
         geom_bar(stat = "identity") + 
         labs(x='MONTH', y= 'Occurrence Count') + 
@@ -128,6 +155,7 @@ make_charts <- function(type_lst, ngbrhd_lst, yr_lst){
             axis.title.x = element_text(size = 15),
             axis.title.y = element_text(size = 15),
             axis.text.x = element_text(angle = 30, hjust = 0.5))
+    ggplotly(chart[1])
 
     chart[2] <- ggplot(TOD, aes(x=HOUR, y=N)) + 
         geom_bar(stat = "identity") + 
@@ -141,6 +169,7 @@ make_charts <- function(type_lst, ngbrhd_lst, yr_lst){
             axis.title.x = element_text(size = 15),
             axis.title.y = element_text(size = 15),
             axis.text.x = element_text(angle = 30, hjust = 0.5))
+    ggplotly(chart[2])
 
 
     chart[3] <- ggplot(crime_rate, aes(x=YEAR, y=rate)) + 
@@ -154,6 +183,7 @@ make_charts <- function(type_lst, ngbrhd_lst, yr_lst){
             text = element_text(size = 12),
             axis.title.x = element_text(size = 15),
             axis.title.y = element_text(size = 15))
+    ggplotly(chart[3])
 
     chart[4] <- ggplot(type_crimes, aes(x=reorder(TYPE, -contri), y=contri)) + 
         geom_bar(stat = "identity") + 
@@ -166,30 +196,53 @@ make_charts <- function(type_lst, ngbrhd_lst, yr_lst){
             axis.title.x = element_text(size = 15),
             axis.title.y = element_text(size = 15),
             axis.text.x = element_text(angle = 30, hjust = 1))
+    ggplotly(chart[4])
 
-    return(plot_grid(chart1, chart2, chart3, chart4))
+    #return(plot_grid(chart1, chart2, chart3, chart4))
 
+    return chart
 }
 
+# Now we define the graph as a dash component using generated figure
+graph <- dccGraph(
+  id = 'gap-graph',
+  figure=make_charts() # gets initial data using argument defaults
+)
 
-type_lst <- unique(mydata$TYPE)
-ngbrhd_lst <- unique(mydata$NEIGHBOURHOOD)
-yr_lst <- unique(mydata$YEAR)
+app$layout(
+  htmlDiv(
+    list(
+      htmlH1('Vancouver Crime Tracker'),
+      htmlH2('This is an interactive visualization based on the data provided by the Vancouver Police Department (VPD)'),
+      #selection components
+      #htmlLabel('Select a year range:'),
+      yearSlider,
+      htmlIframe(height=15, width=10, style=list(borderWidth = 0)), #space
+      #htmlLabel('Select continents:'),
+      neigh_Dropdown,
+      #htmlLabel('Select y-axis metric:'),
+      type_Dropdown,
+      #graph and table
+      graph, 
+      htmlIframe(height=20, width=10, style=list(borderWidth = 0)), #space
+      #htmlLabel('Try sorting by table columns!'),
+      #table,
+      htmlIframe(height=20, width=10, style=list(borderWidth = 0)), #space
+      #dccMarkdown("[Data Source](https://cran.r-project.org/web/packages/gapminder/README.html)")
+    )
+  )
+)
 
-make_charts(type_lst[1:3], ngbrhd_lst, yr_lst)
+
+
+
+# type_lst <- unique(mydata$TYPE)
+# ngbrhd_lst <- unique(mydata$NEIGHBOURHOOD)
+# yr_lst <- unique(mydata$YEAR)
+
+# make_charts(type_lst[1:3], ngbrhd_lst, yr_lst)
 
 # try running each chart in ggplotly
 
 
-
-
-
-
-# Need to add this
-# # Converting XY UTM coordinate system to Latitude & Longitude
-# p = Proj(proj='utm', zone=10, ellps='WGS84', preserve_units=False)
-# lon, lat = p(mydata['X'].values, mydata['Y'].values, inverse=True)
-# latlon = pd.DataFrame(np.c_[lon, lat], columns=['Lon', 'Lat'])
-# mydata['Lon'] = latlon['Lon']
-# mydata['Lat'] = latlon['Lat']
 
