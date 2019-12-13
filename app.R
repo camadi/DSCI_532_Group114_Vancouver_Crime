4c2904db8631d794e4c3b01fab833749ce5573e4
sverma92 <vermashivam679@gmail.com>
2019-12-12 18:53:46 -0800

    Edited content and style

--- app.R
+++ app.R
@@ -1,12 +1,9 @@
 library(tidyverse)
-library(cowplot)
 library(plotly)
 library(dash)
 library(dashCoreComponents)
 library(dashHtmlComponents)
 library(dashTable)
-library(tidyverse)
-library(gapminder)
 
 app <- Dash$new(external_stylesheets = "https://codepen.io/chriddyp/pen/bWLwgP.css")
 
@@ -72,8 +69,7 @@ yearSlider <- dccRangeSlider(
   min = 2003,
   max = 2018,
   step = 5,
-  value = list(2010, 2018)
-  
+  value = seq(2010, 2018, 1)
 )
 
 neigh_Dropdown <- dccDropdown(
@@ -120,124 +116,54 @@ make_charts1 <- function(yr_lst = all_year, ngbrhd_lst = all_neig, type_lst = al
     MOY <- MOY %>% 
         mutate(MONTH_NAME = c('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'))
 
-    TOD <- df %>% 
-        group_by(HOUR) %>%
-        summarise(N = n()) %>%
-        arrange(HOUR)
-
-    type_crimes <- df %>% 
-        group_by(TYPE) %>%
-        summarise(N = n()) %>%
-        arrange(-N)
-    type_crimes <- type_crimes %>% mutate(contri = N/sum(type_crimes$N))
-
-    crime_rate <- df %>% 
-        group_by(YEAR) %>%
-        summarise(N = n()) %>%
-        arrange(YEAR)
-
-
-
-    # Adding population data to plot crime rate
-    required_prop <-pop_prop %>% filter(NEIGHBOURHOOD %in% ngbrhd_lst) %>% pull(proportion) %>% sum()
-    pop_yr <- pop_yr %>% filter(YEAR %in% yr_lst) %>% mutate(Population = Population*required_prop)
-
-    crime_rate <- inner_join(crime_rate, pop_yr)
-    crime_rate <- crime_rate %>% mutate(rate = (N/Population)*1000)
 
     #chart = {}
-    chart1 <- ggplot(MOY, aes(x=reorder(MONTH_NAME, MONTH), y=N)) + 
-        geom_bar(stat = "identity") + 
+    chart1 <- ggplot(MOY, aes(x=reorder(MONTH_NAME, MONTH), y=N, group=1, text=paste("Month:", MONTH_NAME, "</br></br>Count:", N))) + 
+        geom_bar(stat = "identity", fill = "#56B4E9") + 
         labs(x='MONTH', y= 'Occurrence Count') + 
         scale_y_continuous(labels = scales::comma) + 
         ggtitle("Crime Occurrence by Month") + 
-        theme_minimal_grid() + 
+        # theme_minimal_grid() + 
         theme(
             text = element_text(size = 12),
             axis.title.x = element_text(size = 15),
             axis.title.y = element_text(size = 15),
             axis.text.x = element_text(angle = 30, hjust = 0.5))
-    ggplotly(chart1)
+    ggplotly(chart1, tooltip="text")
 
 }
 make_charts2 <- function(yr_lst = all_year, ngbrhd_lst = all_neig, type_lst = all_types){
     df <- mydata %>% 
         filter(TYPE %in% type_lst & NEIGHBOURHOOD %in% ngbrhd_lst & YEAR %in% yr_lst)
 
-    MOY <- df %>% 
-        group_by(MONTH) %>%
-        summarise(N = n()) %>%
-        arrange(MONTH)
-    MOY <- MOY %>% 
-        mutate(MONTH_NAME = c('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'))
-
     TOD <- df %>% 
         group_by(HOUR) %>%
         summarise(N = n()) %>%
         arrange(HOUR)
 
-    type_crimes <- df %>% 
-        group_by(TYPE) %>%
-        summarise(N = n()) %>%
-        arrange(-N)
-    type_crimes <- type_crimes %>% mutate(contri = N/sum(type_crimes$N))
 
-    crime_rate <- df %>% 
-        group_by(YEAR) %>%
-        summarise(N = n()) %>%
-        arrange(YEAR)
-
-
-
-    # Adding population data to plot crime rate
-    required_prop <-pop_prop %>% filter(NEIGHBOURHOOD %in% ngbrhd_lst) %>% pull(proportion) %>% sum()
-    pop_yr <- pop_yr %>% filter(YEAR %in% yr_lst) %>% mutate(Population = Population*required_prop)
-
-    crime_rate <- inner_join(crime_rate, pop_yr)
-    crime_rate <- crime_rate %>% mutate(rate = (N/Population)*1000)
-
-    chart2 <- ggplot(TOD, aes(x=HOUR, y=N)) + 
-        geom_bar(stat = "identity") + 
+    chart2 <- ggplot(TOD, aes(x=HOUR, y=N, group=1, text=paste("Hour:", HOUR, "</br></br>Count:", N))) + 
+        geom_bar(stat = "identity", fill = "#56B4E9") + 
         labs(x='HOUR', y= 'Occurrence Count') + 
         scale_y_continuous(labels = scales::comma) +
         scale_x_continuous(breaks = seq(0, 23, 2)) +
         ggtitle("Crime Occurrence by Time of Day") + 
-        theme_minimal_grid() + 
+        # theme_minimal_grid() + 
         theme(
             text = element_text(size = 12),
             axis.title.x = element_text(size = 15),
             axis.title.y = element_text(size = 15),
             axis.text.x = element_text(angle = 30, hjust = 0.5))
-    ggplotly(chart2)
+    ggplotly(chart2, tooltip="text")
 }
 make_charts3 <- function(yr_lst = all_year, ngbrhd_lst = all_neig, type_lst = all_types){
     df <- mydata %>% 
         filter(TYPE %in% type_lst & NEIGHBOURHOOD %in% ngbrhd_lst & YEAR %in% yr_lst)
 
-    MOY <- df %>% 
-        group_by(MONTH) %>%
-        summarise(N = n()) %>%
-        arrange(MONTH)
-    MOY <- MOY %>% 
-        mutate(MONTH_NAME = c('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'))
-
-    TOD <- df %>% 
-        group_by(HOUR) %>%
-        summarise(N = n()) %>%
-        arrange(HOUR)
-
-    type_crimes <- df %>% 
-        group_by(TYPE) %>%
-        summarise(N = n()) %>%
-        arrange(-N)
-    type_crimes <- type_crimes %>% mutate(contri = N/sum(type_crimes$N))
-
     crime_rate <- df %>% 
         group_by(YEAR) %>%
         summarise(N = n()) %>%
         arrange(YEAR)
-
-
 
     # Adding population data to plot crime rate
     required_prop <-pop_prop %>% filter(NEIGHBOURHOOD %in% ngbrhd_lst) %>% pull(proportion) %>% sum()
@@ -246,34 +172,24 @@ make_charts3 <- function(yr_lst = all_year, ngbrhd_lst = all_neig, type_lst = al
     crime_rate <- inner_join(crime_rate, pop_yr)
     crime_rate <- crime_rate %>% mutate(rate = (N/Population)*1000)
 
-    chart3 <- ggplot(crime_rate, aes(x=YEAR, y=rate)) + 
+    chart3 <- ggplot(crime_rate, aes(x=YEAR, y=rate, group=1, text=paste("Year:", YEAR, "</br></br>Rate:", rate))) + 
         geom_point() + 
-        geom_line() + 
+        geom_line(colour = "#56B4E9") + 
         labs(x='YEAR', y= 'Crime Occurrences per 1000 People') + 
         scale_x_continuous(breaks = seq(min(crime_rate$YEAR), max(crime_rate$YEAR), 1)) +
         ggtitle("Crime Rate") + 
-        theme_minimal_grid() + 
+        # theme_minimal_grid() + 
         theme(
             text = element_text(size = 12),
             axis.title.x = element_text(size = 15),
             axis.title.y = element_text(size = 15))
-    ggplotly(chart3)
+    ggplotly(chart3, tooltip="text")
 }
+
  make_charts4 <- function(yr_lst = all_year, ngbrhd_lst = all_neig, type_lst = all_types){
     df <- mydata %>% 
         filter(TYPE %in% type_lst & NEIGHBOURHOOD %in% ngbrhd_lst & YEAR %in% yr_lst)
 
-    MOY <- df %>% 
-        group_by(MONTH) %>%
-        summarise(N = n()) %>%
-        arrange(MONTH)
-    MOY <- MOY %>% 
-        mutate(MONTH_NAME = c('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'))
-
-    TOD <- df %>% 
-        group_by(HOUR) %>%
-        summarise(N = n()) %>%
-        arrange(HOUR)
 
     type_crimes <- df %>% 
         group_by(TYPE) %>%
@@ -281,32 +197,19 @@ make_charts3 <- function(yr_lst = all_year, ngbrhd_lst = all_neig, type_lst = al
         arrange(-N)
     type_crimes <- type_crimes %>% mutate(contri = N/sum(type_crimes$N))
 
-    crime_rate <- df %>% 
-        group_by(YEAR) %>%
-        summarise(N = n()) %>%
-        arrange(YEAR)
-
-
-
-    # Adding population data to plot crime rate
-    required_prop <-pop_prop %>% filter(NEIGHBOURHOOD %in% ngbrhd_lst) %>% pull(proportion) %>% sum()
-    pop_yr <- pop_yr %>% filter(YEAR %in% yr_lst) %>% mutate(Population = Population*required_prop)
-
-    crime_rate <- inner_join(crime_rate, pop_yr)
-    crime_rate <- crime_rate %>% mutate(rate = (N/Population)*1000)
-
-    chart4 <- ggplot(type_crimes, aes(x=reorder(TYPE, -contri), y=contri)) + 
-        geom_bar(stat = "identity") + 
+    chart4 <- ggplot(type_crimes, aes(x=reorder(TYPE, contri), y=contri, group=1, text=paste("Crime Type:", TYPE, "</br></br>Contribution:", contri))) + 
+        geom_bar(stat = "identity", fill = "#56B4E9") + 
         labs(x='', y= 'Contribution') + 
         scale_y_continuous(labels = scales::percent) +
         ggtitle("Constituents of Selected Crimes") + 
-        theme_minimal_grid() + 
+        # theme_minimal_grid() + 
         theme(
             text = element_text(size = 12),
             axis.title.x = element_text(size = 15),
             axis.title.y = element_text(size = 15),
-            axis.text.x = element_text(angle = 30, hjust = 1))
-    ggplotly(chart4)
+            axis.text.x = element_text(hjust = 1)) + 
+        coord_flip()
+    ggplotly(chart4, tooltip="text")
 }
     #return(plot_grid(chart1, chart2, chart3, chart4))
     #return chart
@@ -315,49 +218,75 @@ make_charts3 <- function(yr_lst = all_year, ngbrhd_lst = all_neig, type_lst = al
 # Now we define the graph as a dash component using generated figure
 graph1 <- dccGraph(
   id = 'graph1',
-  figure=make_charts1() # gets initial data using argument defaults
+  figure=make_charts1(), # gets initial data using argument defaults
+  style = list(
+        width ='49%',
+        display = 'inline-block',
+        padding = '0 20')
 )
 
 graph2 <- dccGraph(
   id = 'graph2',
-  figure=make_charts2() # gets initial data using argument defaults
+  figure=make_charts2(), # gets initial data using argument defaults
+  style = list(
+        width ='49%',
+        display = 'inline-block',
+        padding = '0 20')
 )
 
 graph3 <- dccGraph(
   id = 'graph3',
-  figure=make_charts3() # gets initial data using argument defaults
+  figure=make_charts3(), # gets initial data using argument defaults
+  style = list(
+        width ='49%',
+        display = 'inline-block',
+        padding = '0 20')
 )
 
 graph4 <- dccGraph(
   id = 'graph4',
-  figure=make_charts4() # gets initial data using argument defaults
+  figure=make_charts4(), # gets initial data using argument defaults
+  style = list(
+        width ='49%',
+        display = 'inline-block',
+        padding = '0 20')
 )
 app$layout(
   htmlDiv(
     list(
-      htmlH1('Vancouver Crime Tracker'),
+      htmlH1('Vancouver Crime Tracker', style = list('color'='#0072B2', 'padding'= '20px 20px')),
       htmlIframe(height=20, width=10, style=list(borderWidth = 0)), #space
-      htmlH3("Using Data from VPD we have created several plots for residents and visitors of Vancouver to view crime data. 
+      htmlH5("Using Data from VPD we have created several plots for residents and visitors of Vancouver to view crime data. 
       There are three selectors, one for neighbourhood, crime type, and year. All three selectors impact each of the plots. 
-      The crime rate plot is normalized for population change in Vancouver, other than that the data is non-normalized."),
+      The crime rate plot is normalized for population change in Vancouver, other than that the data is non-normalized.", style = list('color'='white', 'background-color'='#0072B2', 'padding'= '20px')),
+
+      htmlIframe(height=50, width=10, style=list(borderWidth = 0)), #space
+
       #selection components
-      #htmlLabel('Select a year range:'),
-      yearSlider,
+      htmlDiv(list(htmlH5('Select Year')), style = list(width = '80%', padding= '20px')),
+      htmlDiv(list(yearSlider), style = list(width = '90%', padding= '20px')),
+      # yearSlider,
+      htmlIframe(height=100, width=10, style=list(borderWidth = 0)), #space
+
+      htmlDiv(list(htmlH5('Select Neighborhood')), style = list(width = '65%', display = 'inline-block')),
       htmlIframe(height=15, width=10, style=list(borderWidth = 0)), #space
-      #htmlLabel('Select continents:'),
-      neigh_Dropdown,
+      # type_Dropdown,
+      htmlDiv(list(htmlH5('Select Type of Crime')), style = list(width = '33%', display = 'inline-block')),
       htmlIframe(height=15, width=10, style=list(borderWidth = 0)), #space
-      type_Dropdown,
+
+
+      htmlDiv(list(neigh_Dropdown), style = list(width = '65%', display = 'inline-block')),
       htmlIframe(height=15, width=10, style=list(borderWidth = 0)), #space
-      graph1, 
-      graph2,
-      graph3,
-      graph4,
-      htmlIframe(height=20, width=10, style=list(borderWidth = 0)), #space
-      #htmlLabel('Try sorting by table columns!'),
-      #table,
-      htmlIframe(height=20, width=10, style=list(borderWidth = 0))#space
-      #dccMarkdown("[Data Source](https://cran.r-project.org/web/packages/gapminder/README.html)")
+      # type_Dropdown,
+      htmlDiv(list(type_Dropdown), style = list(width = '33%', display = 'inline-block')),
+
+      htmlDiv(list(graph1, htmlIframe(width=20, style=list(borderWidth = 0)), graph2), style = list(width = '80%', padding= '100px 100px')),
+
+      htmlDiv(list(graph3, htmlIframe(width=20, style=list(borderWidth = 0)), graph4), style = list(width = '90%', padding= '50px 90px')),
+
+      htmlIframe(height=200, width=10, style=list(borderWidth = 0)),
+      dccMarkdown("[Data Source](https://geodash.vpd.ca/opendata/)"),
+      dccMarkdown("*This Dashbord was made collaboratively by Chimaobi, Elliott, Kirk, and Shivam*")
     )
   )
 )
